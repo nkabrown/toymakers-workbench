39016ac318ad4ebba8bc31ddd2b4b18b75be01b7
Nathan Brown <nkabrown@gmail.com>
2016-10-17 21:14:52 -0400

    react smoketest

---
+++ hello-from-react/src/components/Input.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Input extends React.Component {
+  render() {
+    return (
+      <p>Input</p>
+    );
+  }
+}
+
+export default Input;
---
+++ hello-from-react/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import Input from './components/Input';
+
+render(<Input />, document.getElementById('mount'));
